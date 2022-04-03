ae4f45df6f52aa9391d4b44cb4775596aae51135
crypto-pool2 <101302548+crypto-pool2@users.noreply.github.com>
2022-04-03 11:02:02 +0800

    Update restart-btc-antpool.bat

--- restart-btc-antpool.bat
+++ restart-btc-antpool.bat
@@ -7,7 +7,7 @@ netstat -ano|findstr %ip%:%port%|findstr -i LISTENING
 if ERRORLEVEL 1 (goto err) else (goto ok)
 
 :err
-start /d "C:\PoolCryptoV2\" PoolCryptoV2.exe local 0.0.0.0:6666 94.103.4.193:16666 BTCANT
+start /d "加密程序所在路径" PoolCryptoV2.exe local 0.0.0.0:自定义端口 节点IP+PORT 私钥
 
 :ok
 echo PoolCryptoV2-Bitcoin-AntPool Services is running %Date:~0,4%-%Date:~5,2%-%Date:~8,2% %Time:~0,2%:%Time:~3,2%
